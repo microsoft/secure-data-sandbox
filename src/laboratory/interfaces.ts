ac9a2248a9171b4736353cd420bcf8b728d3d64c
Michael Hopcroft <mhop@microsoft.com>
2020-02-21 14:47:04 -0800

    Sequelize ORM prototype

---
+++ src/laboratory/interfaces.ts
@@ -0,0 +1,88 @@
+// tslint:disable-next-line:interface-name
+export interface IEntityBase {
+    name: string;
+    author: string;
+    version: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// IBenchmark
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// tslint:disable-next-line:interface-name
+export interface IBenchmark extends IEntityBase {
+    pipelines: IPipeline[];
+    // TODO: results table definition.
+}
+
+// tslint:disable-next-line:interface-name
+export interface IPipeline {
+    mode: string;
+    stages: PipelineStage[];
+};
+
+type PipelineStage = ICandidateStage | IContainerStage;
+
+// tslint:disable-next-line:interface-name
+export interface ICandidateStage {
+};
+
+// tslint:disable-next-line:interface-name
+export interface IContainerStage {
+    image: string;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// ISuite
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// tslint:disable-next-line:interface-name
+export interface ISuite extends IEntityBase {
+    benchmark: string;
+    mode: string;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// ICandidate
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// tslint:disable-next-line:interface-name
+export interface ICandidate extends IEntityBase {
+    benchmark: string;
+    mode: string;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// IRunRequest
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// tslint:disable-next-line:interface-name
+export interface IRunRequest extends IEntityBase {
+    benchmark: IBenchmark;
+    suite: ISuite;
+    candidate: ICandidate;
+    status: RunStatus;
+}
+
+export enum RunStatus {
+    CREATED = 'created',
+    RUNNING = 'running',
+    COMPLETED = 'completed',
+    FAILED = 'failed'
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Results will be dynamically typed.
+//
+///////////////////////////////////////////////////////////////////////////////
