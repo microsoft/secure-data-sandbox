b8ef253c22836444f49f50374e34343e8b907568
Noel Bundick <noelbundick@gmail.com>
2020-03-02 15:50:14 -0800

    fix CREATE USER command

--- deploy/azure/Bootstrap-Database.ps1
+++ deploy/azure/Bootstrap-Database.ps1
@@ -6,21 +6,18 @@ function Bootstrap-Database {
   $conn = new-object System.Data.SqlClient.SqlConnection;
   $conn.ConnectionString = "Data Source=${Env:SERVER}; Initial Catalog=${Env:DATABASE};";
   $conn.AccessToken = $token.access_token;
-  
-  $sql = @"
-create user [@identity] with default_schema=[dbo], SID=@sid, TYPE=E;
-alter role db_ddladmin add member [@identity];
-alter role db_datareader add member [@identity];
-alter role db_datawriter add member [@identity];
-"@
-  $cmd = new-object System.Data.SqlClient.SqlCommand($sql, $conn)
-  $cmd.Parameters.AddWithValue("@identity", $Env:IDENTITY)
+  $conn.Open()
   
   foreach ($byte in [System.Guid]::Parse($Env:CLIENT_ID).ToByteArray()) { $byteGuid += [System.String]::Format("{0:X2}", $byte) }
-  $cmd.Parameters.AddWithValue("@sid", "0x$byteGuid")
-
-  $conn.Open()
+  $sql = @"
+create user [$Env:IDENTITY] with default_schema=[dbo], SID=$byteGuid, TYPE=E;
+alter role db_ddladmin add member [$Env:IDENTITY];
+alter role db_datareader add member [$Env:IDENTITY];
+alter role db_datawriter add member [$Env:IDENTITY];
+"@
+  $cmd = new-object System.Data.SqlClient.SqlCommand($sql, $conn)
   $cmd.ExecuteNonQuery()
+
   $conn.Close()
 }
 
