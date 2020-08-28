27ff8070f2217d60ed0eafa2234afc8bbfc65074
Noel Bundick <noelbundick@gmail.com>
2020-08-28 03:15:16 +0000

    Add dev vm script

---
+++ scripts/dev-vm-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+# Useful tools for developing / debugging on a remote terminal connected to the detonation chamber
+
+# Azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# kubectl
+az aks install-cli
+
+# helm
+sudo snap install helm --classic
+
+# octant
+curl -LO https://github.com/vmware-tanzu/octant/releases/download/v0.15.0/octant_0.15.0_Linux-64bit.tar.gz
+tar xvf octant_0.15.0_Linux-64bit.tar.gz
+sudo mv ./octant_0.15.0_Linux-64bit/octant /usr/local/bin
