df7bfef3b2b5bbd1602726bf6cdd05e4e46488c8
Ubuntu <azureuser@dev.spctxj15lqeu3g04bkji4epajh.jx.internal.cloudapp.net>
2020-09-23 04:05:14 +0000

    improve dev vm config

--- scripts/dev-vm-setup.sh
+++ scripts/dev-vm-setup.sh
@@ -14,7 +14,12 @@ sudo az aks install-cli
 sudo snap install helm --classic
 
 # Docker
+sudo addgroup --system docker
+sudo adduser $USER docker
+newgrp docker
+
 sudo snap install docker
+sudo snap start docker
 
 # Node
 sudo snap install node --classic --channel=12
