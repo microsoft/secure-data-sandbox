0c901ea325a5db90d32509acefb42a9e65e034f1
Noel Bundick <noelbundick@gmail.com>
2020-10-21 19:56:19 +0000

    Import public images to infra registry

--- deploy/deploy-acr.sh
+++ deploy/deploy-acr.sh
@@ -5,12 +5,21 @@ RESOURCE_GROUP=${1:-sds}
 DEPLOYMENT_NAME=${2:-azuredeploy}
 TAG=${3:-latest}
 
+# Versions
+ARGO_VERSION="v2.11.0"
+
 export DOCKER_BUILDKIT=1
 
 OUTPUTS=$(az deployment group show -g $RESOURCE_GROUP -n $DEPLOYMENT_NAME --query properties.outputs -o json)
 LOGIN_SERVER=$(echo $OUTPUTS | jq -r .infraAcrLoginServer.value)
 az acr login -n $LOGIN_SERVER
 
+# Import public images
+az acr import -n $LOGIN_SERVER --source "argoproj/argoexec:$ARGO_VERSION"
+az acr import -n $LOGIN_SERVER --source "argoproj/workflow-controller:$ARGO_VERSION"
+az acr import -n $LOGIN_SERVER --source "argoproj/argocli:$ARGO_VERSION"
+
+# Push SDS images
 LABORATORY_IMAGE="${LOGIN_SERVER}/sds-laboratory:${TAG}"
 docker build --target laboratory -t $LABORATORY_IMAGE .
 docker push $LABORATORY_IMAGE
--- deploy/deploy-aks.sh
+++ deploy/deploy-aks.sh
@@ -4,23 +4,31 @@ set -euo pipefail
 RESOURCE_GROUP=${1:-sds}
 DEPLOYMENT_NAME=${2:-azuredeploy}
 
+# Versions
+AAD_POD_IDENTITY_VERSION="2.0.2"
+AZUREFILE_CSI_DRIVER_VERSION="v0.9.0"
+ARGO_VERSION="v2.11.0"
+
 # aad-pod-identity
 if ! helm repo list | grep '^aad-pod-identity'; then
   helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
 fi
-helm upgrade --install aad-pod-identity aad-pod-identity/aad-pod-identity --namespace kube-system
+helm upgrade --install aad-pod-identity aad-pod-identity/aad-pod-identity --namespace kube-system --version $AAD_POD_IDENTITY_VERSION --set nmi.metadataHeaderRequired=true
 
 # azurefile-csi-driver
 if ! helm repo list | grep '^azurefile-csi-driver'; then
   helm repo add azurefile-csi-driver https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/charts
 fi
-helm upgrade --install azurefile-csi-driver azurefile-csi-driver/azurefile-csi-driver --namespace kube-system
+helm upgrade --install azurefile-csi-driver azurefile-csi-driver/azurefile-csi-driver --namespace kube-system --version $AZUREFILE_CSI_DRIVER_VERSION
 
-
+INFRA_ACR=$(echo $OUTPUTS | jq -r .infraAcrLoginServer.value)
 OUTPUTS=$(az deployment group show -g $RESOURCE_GROUP -n $DEPLOYMENT_NAME --query properties.outputs -o json)
 helm upgrade --install sds deploy/helm \
   --set "instrumentationKey=$(echo $OUTPUTS | jq -r .instrumentationKey.value)" \
-  --set "worker.image=$(echo $OUTPUTS | jq -r .infraAcrLoginServer.value)/sds-worker" \
+  --set "argoController.executorImage=$INFRA_ACR/argoexec:$ARGO_VERSION" \
+  --set "argoController.image=$INFRA_ACR/workflow-controller:$ARGO_VERSION" \
+  --set "argoServer.image=$INFRA_ACR/argocli:$ARGO_VERSION" \
+  --set "worker.image=$INFRA_ACR/sds-worker" \
   --set "worker.queueEndpoint=$(echo $OUTPUTS | jq -r .runsQueueEndpoint.value)" \
   --set "identities.worker.resourceId=$(echo $OUTPUTS | jq -r .workerIdentityResourceId.value)" \
   --set "identities.worker.clientId=$(echo $OUTPUTS | jq -r .workerIdentityClientId.value)" \
